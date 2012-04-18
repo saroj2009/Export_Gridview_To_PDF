a117cebba65d6d2d0bc7dd8f1f1c02df96d191f3
unknown <SAROJ@.(none)>
2012-04-18 13:24:39 +0530

    c3

---
+++ ExportGridviewtoPDF/Default.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.html.simpleparser;
+
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        gvdetails.DataSource = GetTable();
+        gvdetails.DataBind();
+    }
+    public DataTable GetTable()
+    {
+        //
+        // Here we create a DataTable with four columns.
+        //
+        DataTable table = new DataTable();
+        table.Columns.Add("Dosage", typeof(int));
+        table.Columns.Add("Drug", typeof(string));
+        table.Columns.Add("Patient", typeof(string));
+        table.Columns.Add("Date", typeof(DateTime));
+
+        //
+        // Here we add five DataRows.
+        //
+        table.Rows.Add(25, "Indocin", "David", DateTime.Now);
+        table.Rows.Add(50, "Enebrel", "Sam", DateTime.Now);
+        table.Rows.Add(10, "Hydralazine", "Christoff", DateTime.Now);
+        table.Rows.Add(21, "Combivent", "Janet", DateTime.Now);
+        table.Rows.Add(100, "Dilantin", "Melanie", DateTime.Now);
+        return table;
+    }
+
+    public override void VerifyRenderingInServerForm(Control control)
+    {
+        /* Verifies that the control is rendered */
+    }
+  
+    protected void btn_pdf_Click(object sender, EventArgs e)
+    {
+        Response.ContentType = "application/pdf";
+        Response.AddHeader("content-disposition", "attachment;filename=UserDetails.pdf");
+        Response.Cache.SetCacheability(HttpCacheability.NoCache);
+        StringWriter sw = new StringWriter();
+        HtmlTextWriter hw = new HtmlTextWriter(sw);
+        gvdetails.AllowPaging = false;
+        gvdetails.DataBind();
+        gvdetails.RenderControl(hw);
+        gvdetails.HeaderRow.Style.Add("width", "15%");
+        gvdetails.HeaderRow.Style.Add("font-size", "10px");
+        gvdetails.Style.Add("text-decoration", "none");
+        gvdetails.Style.Add("font-family", "Arial, Helvetica, sans-serif;");
+        gvdetails.Style.Add("font-size", "8px");
+        StringReader sr = new StringReader(sw.ToString());
+        Document pdfDoc = new Document(PageSize.A2, 7f, 7f, 7f, 0f);
+        HTMLWorker htmlparser = new HTMLWorker(pdfDoc);
+        PdfWriter.GetInstance(pdfDoc, Response.OutputStream);
+        pdfDoc.Open();
+        htmlparser.Parse(sr);
+        pdfDoc.Close();
+        Response.Write(pdfDoc);
+        Response.End();
+    }
+}
